d6a1d073af8f181f7a69d8308b86b3f5bfdb52f4
josedavidmarin <162277695+josedavidmarin@users.noreply.github.com>
2024-03-08 14:53:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import ui
-import api
+from ui.ui import obtener_parametros, mostrar_resultados
+from api.api import consultar_datos
 
 def main():
-    departamento, municipio, cultivo, limit = ui.obtener_parametros()
-    tabla, mediana_variables_edaficas = api.consultar_datos(departamento, municipio, cultivo, limit)
+    departamento, municipio, cultivo, limit = obtener_parametros()
+    tabla = consultar_datos(departamento, municipio, cultivo, limit)
     if tabla is not None:
-        ui.mostrar_resultados(tabla, mediana_variables_edaficas)
+        mostrar_resultados(tabla)
     else:
         print("Error al consultar los datos.")
 
