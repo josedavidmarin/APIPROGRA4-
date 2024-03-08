cec9a9e25d964587711e0673c9a7b9beac92a5c7
josedavidmarin <162277695+josedavidmarin@users.noreply.github.com>
2024-03-08 14:53:23 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,38 +1,29 @@
-#!/ usr/bin/env python
-# make sure to install these packages before running :
-# pip install pandas
-# pip install sodapy
-import pandas as pd
 from sodapy import Socrata
- # Unauthenticated client only works with public data sets . Note ’None ’
- # in place of application token , and no username or password :
-# client = Socrata ("www.datos.gov.co", None )
+import pandas as pd
 
- # Example authenticated client ( needed for non - public datasets ):
- # client = Socrata (www. datos .gov.co ,
- # MyAppToken ,
- # username =" user@example . com" ,
- # password =" AFakePassword ")
+def consultar_datos(departamento, municipio, cultivo, limit):
+    # Cliente para interactuar con la API del Portal Nacional de Datos Abiertos
+    client = Socrata("www.datos.gov.co", None)
 
- # First 2000 results , returned as JSON from API / converted to Python
-# list of
-# dictionaries by sodapy .
-# results = client.get("https://www.datos.gov.co/resource/ch4u-f3i5.json", limit =1000)
-# Convert to pandas DataFrame
-#  i need make a get of this https://www.datos.gov.co/resource/ch4u-f3i5.json
+    # Obtener los resultados de la API
+    results = client.get("ch4u-f3i5", limit=limit)
 
-soda = Socrata("www.datos.gov.co", None)
-results = soda.get("ch4u-f3i5", limit=46745)
+    # Convertir los resultados a un DataFrame de Pandas
+    results_df = pd.DataFrame.from_records(results)
 
-#print(results)
+    # Filtrar los resultados según el departamento, municipio y cultivo
+    resultados_filtrados = results_df[(results_df['departamento'] == departamento) & 
+                                      (results_df['municipio'] == municipio) & 
+                                      (results_df['cultivo'] == cultivo)]
 
+    # Extraer la tabla con las columnas requeridas
+    tabla = resultados_filtrados[['departamento', 'municipio', 'cultivo', 'topografia']]
+    
 
-def obtenerTabla(dep, mun, cult):
-    for r in results:
-        if(r["departamento"] == dep and r["municipio"] == mun and r["cultivo"] == cult):
-            # prom = (float(r["ph_agua_suelo_2_5_1_0"]) + float(r["f_sforo_p_bray_ii_mg_kg"]) + float(r["potasio_k_intercambiable_cmol_kg"])) / 3
-            print([r["departamento"], r["municipio"], r["cultivo"], r["topografia"]])
-            
+    # Calcular la mediana de las variables edáficas
+    mediana = (resultados_filtrados['ph_agua_suelo_2_5_1_0'].apply(lambda x: float(x)) + resultados_filtrados['f_sforo_p_bray_ii_mg_kg'].apply(lambda x: float(x)) + resultados_filtrados['potasio_k_intercambiable_cmol_kg'].apply(lambda x: float(x)))/3
+    tabla['media'] = mediana
 
+    
 
-obtenerTabla("ANTIOQUIA", "ABEJORRAL", "Aguacate")
+    return tabla
