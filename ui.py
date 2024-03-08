94d2f3d32367e46d855b95f025837381a89fbc2a
josedavidmarin <162277695+josedavidmarin@users.noreply.github.com>
2024-03-08 14:54:10 -0500

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,13 +1,10 @@
 def obtener_parametros():
-    dep = input("Ingrese el nombre del departamento: ")
-    mun = input("Ingrese el nombre del municipio: ")
-    cult = input("Ingrese el nombre del cultivo: ")
+    departamento = input("Ingrese el nombre del departamento: ")
+    municipio = input("Ingrese el nombre del municipio: ")
+    cultivo = input("Ingrese el nombre del cultivo: ")
     limit = int(input("Ingrese el número de registros a consultar: "))
-    return dep, mun, cult, limit
+    return departamento, municipio, cultivo, limit
 
-def mostrar_resultados(tabla, mediana_variables_edaficas):
+def mostrar_resultados(tabla):
     print("Tabla de resultados:")
     print(tabla)
-    print("\nMediana de las variables edáficas:")
-    for variable, mediana in mediana_variables_edaficas.items():
-        print(f"{variable}: {mediana}")
